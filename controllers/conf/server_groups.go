fdf65ff7ea87067dcdeb01b33a7f8d8d0a3ca4af
Davidey.Lee <liwanwei@zto.cn>
2020-02-20 14:37:23 +0800

    补充配置

---
+++ controllers/conf/server_groups.go
@@ -0,0 +1,26 @@
+package confcontrollers
+
+import (
+	"github.com/astaxie/beego"
+	"kuaidian-app/controllers"
+	"kuaidian-app/library/jumpserver"
+)
+
+type ServerGroupsController struct {
+	controllers.BaseController
+}
+
+func (c *ServerGroupsController) Get() {
+	if c.User == nil || c.User.Id == 0 {
+		c.SetJson(2, nil, "not login")
+		return
+	}
+	enableJumpserver, _ := beego.AppConfig.Bool("enableJumpserver")
+	if enableJumpserver == true {
+		group2id, _ := jumpserver.GetGroups()
+		c.SetJson(0, group2id, "")
+	} else {
+		c.SetJson(0, nil, "")
+	}
+	return
+}
